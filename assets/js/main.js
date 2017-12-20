751c1f0554058bcc161ca436eea8a83507dc5a18
KamiRicky <rickypeng99@gmail.com>
2017-12-19 23:35:00 -0600

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "rickypeng99");
+    new GitHubCalendar("#github-graph", "rickypeng99");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
