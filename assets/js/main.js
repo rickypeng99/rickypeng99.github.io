867a3579c01d80ab2a3f3e2b8914f8d528c8cc66
KamiRicky <rickypeng99@gmail.com>
2017-12-19 19:53:52 -0600

    twitter

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,5 +61,13 @@ jQuery(document).ready(function($) {
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "rickypeng99", selector: "#ghfeed" });
 
+    twttr.widgets.createTimeline(
+  {
+    sourceType: "profile",
+    screenName: "TwitterDev"
+  },
+  document.getElementById("container")
+);
+
 
 });
